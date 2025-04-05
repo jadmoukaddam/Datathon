6772226e28d49e6e8825a3e1e0bf5098bff541de
unknown <benlee@ethz.ch>
2025-04-05 20:43:53 +0200

    updated flatdf with nationality from client profile instead of passport

--- flatdf.py
+++ flatdf.py
@@ -9,8 +9,8 @@ def data_to_df(full_data:list):
     dfs = []
     for client in full_data:
         keep = [
-            ['gender', 'country_code', 'nationality', 'birth_date'],
-            ['country_of_domicile', 'marital_status',
+            ['gender', 'country_code', 'birth_date'],
+            ['country_of_domicile', 'nationality', 'marital_status',
              'higher_education', 'employment_history', 'aum', 'inheritance_details',
              'real_estate_details', 'investment_risk_profile', 'investment_horizon', 'investment_experience',
              'type_of_mandate', 'preferred_markets', 'currency'],
